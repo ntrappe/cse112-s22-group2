7bd418508a86d37d497ee4919fab9cfa8c792273
Nicole Trappe <ntrappe@ucsd.edu>
2022-05-02 15:44:12 -0700

    added fake JS file to test docs workflow

---
+++ main.js
@@ -0,0 +1,11 @@
+/**
+ * @method
+ * To print out hello world some number of times
+ * @param {Number} how many times to print
+ */
+helloWorld = (n) => {
+    for (let i = 0; i < n; i++)
+        console.log ("Hello World!");
+}
+
+helloWorld (2);

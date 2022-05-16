3663d4f9a68292c869c49799ba5ec0bf254143fe
Nicole Trappe <ntrappe@ucsd.edu>
2022-05-15 21:55:16 -0700

    fixed linting

--- source/add-component.js
+++ source/add-component.js
@@ -5,10 +5,10 @@
 import DailyLog from './components/daily-log/daily-log.js';
 
 /* grab the point on the HTML page to add component to */
-const main = document.getElementById ('main');
+const main = document.getElementById('main');
 /* create an instance of the custom component DailyLog */
-const dailyLog = new DailyLog ();
+const dailyLog = new DailyLog();
 /* append 1 daily log to the page */
-main.appendChild (dailyLog);
+main.appendChild(dailyLog);
 /* append another daily log to the page */
 // main.appendChild (dailyLog);

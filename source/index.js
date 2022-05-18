300a1aa86a1ab1cf710c87b9e9dbc6b3b3da2862
b1hu <jackhubaitong@gmail.com>
2022-05-18 11:57:59 -0700

    delete button update
    add delete button fix minor css issue

--- source/index.js
+++ source/index.js
@@ -1,5 +1,9 @@
 let editBtn = document.getElementById('edit-btn');
 let checkboxes = document.querySelectorAll('input[type=checkbox]');
+let deleteAllBtn = document.getElementById('delete_all_btn');
+let deleteBtn = document.getElementById('delete_btn');
+let updateInfo = document.getElementById('update_info');
+let editIcon = document.getElementById('edit_icon');
 
 editBtn.addEventListener('click', toggleCheckboxDisplay);
 
@@ -16,6 +20,12 @@ function toggleCheckboxDisplay(){
             checkbox.checked = false;
             checkbox.style.display = 'inline-block';
         });
+
+        updateInfo.style.display = 'none';
+        editIcon.style.display = 'none';
+        deleteAllBtn.style.display = 'block';
+        deleteBtn.style.display = 'block';
+        
     }
     // stop showing checkboxes when cancel is clicked and set text back to edit
     else{
@@ -23,5 +33,11 @@ function toggleCheckboxDisplay(){
         checkboxes.forEach(function(checkbox){
             checkbox.style.display = 'none';
         });
+
+        updateInfo.style.display = 'block';
+        editIcon.style.display = 'block';
+        delete_all_btn.style.display = 'none';
+        delete_btn.style.display = 'none';
     }
+
 }

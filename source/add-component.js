5ef03bdb335c2d58d0d4a7c26d8c101ebc72325d
YiyueMaggieMao <myykitty711@gmail.com>
2022-05-20 12:36:21 -0700

    Fixed linting

--- source/add-component.js
+++ source/add-component.js
@@ -2,46 +2,46 @@
  * @module add-component
  */
 
-import DailyLogPreview from "./components/daily-log-preview/daily-log-preview.js";
+import DailyLogPreview from './components/daily-log-preview/daily-log-preview.js';
 
 /* Sample code of how to render a custom component */
 const data = [
-	{
-		dateOfEntry: "04/17/2022",
-		textEntry:
-			"We are no strangers to love. You know the rules, and so do I. A full commitment's what I'm thinking of, you wouldn't get this from any other guy",
-		didTrackers: false,
-		didNotes: true,
-		didJournal: false,
-	},
-	{
-		dateOfEntry: "04/30/2022",
-		textEntry: "",
-		didTrackers: true,
-		didNotes: false,
-		didJournal: true,
-	},
-	{
-		dateOfEntry: "05/19/2022",
-		textEntry: "Never gonna give you up, never gonna let you down",
-		didTrackers: true,
-		didNotes: true,
-		didJournal: true,
-	},
+    {
+        dateOfEntry: '04/17/2022',
+        textEntry:
+            'We are no strangers to love. You know the rules, and so do I. A full commitment\'s what I\'m thinking of, you wouldn\'t get this from any other guy',
+        didTrackers: false,
+        didNotes: true,
+        didJournal: false,
+    },
+    {
+        dateOfEntry: '04/30/2022',
+        textEntry: '',
+        didTrackers: true,
+        didNotes: false,
+        didJournal: true,
+    },
+    {
+        dateOfEntry: '05/19/2022',
+        textEntry: 'Never gonna give you up, never gonna let you down',
+        didTrackers: true,
+        didNotes: true,
+        didJournal: true,
+    },
 ];
 
 /* grab the point on the HTML page to add component to */
-const main = document.getElementById("main");
+const main = document.getElementById('main');
 
 /* Use a loop to dynamically render the components */
-data.forEach(function (dailyLog) {
-	const dailyLogPreview = new DailyLogPreview();
-	main.appendChild(dailyLogPreview);
-	dailyLogPreview.populateFields(
-		dailyLog.dateOfEntry,
-		dailyLog.textEntry,
-		dailyLog.didTrackers,
-		dailyLog.didNotes,
-		dailyLog.didJournal
-	);
+data.forEach((dailyLog) => {
+    const dailyLogPreview = new DailyLogPreview();
+    main.appendChild(dailyLogPreview);
+    dailyLogPreview.populateFields(
+        dailyLog.dateOfEntry,
+        dailyLog.textEntry,
+        dailyLog.didTrackers,
+        dailyLog.didNotes,
+        dailyLog.didJournal,
+    );
 });
--- source/components/daily-log-preview/daily-log-preview.js
+++ source/components/daily-log-preview/daily-log-preview.js
@@ -1,22 +1,22 @@
 // Class name constants
-const DAILY_LOG_PREVIEW_WRAPPER_CLASS = "daily-log-preview-wrapper";
-const TEXT_WRAPPER_CLASS = "text-container";
-const ICONS_WRAPPER_CLASS = "icon-container";
-const DAILY_LOG_TITLE = "Daily Log ";
+const DAILY_LOG_PREVIEW_WRAPPER_CLASS = 'daily-log-preview-wrapper';
+const TEXT_WRAPPER_CLASS = 'text-container';
+const ICONS_WRAPPER_CLASS = 'icon-container';
+const DAILY_LOG_TITLE = 'Daily Log ';
 
 // Preview constants
-const NO_PREVIEW_TEXT = "No preivew text available.";
+const NO_PREVIEW_TEXT = 'No preivew text available.';
 const MAX_PREVIEW_LENGTH = 75;
 
 // Icon constants
-const TRACKER_ICON = "./icons/tracker";
-const NOTES_ICON = "./icons/notes";
-const JOURNAL_ICON = "./icons/journal";
-const ON = "-icon-on.png";
-const OFF = "-icon-off.png";
-const NO_TRACKER_FILLED_OUT = "no trackers filled out";
-const NO_NOTES_FILLED_OUT = "no notes filled out";
-const NO_JOURNAL_FILLED_OUT = "no journal filled out";
+const TRACKER_ICON = './icons/tracker';
+const NOTES_ICON = './icons/notes';
+const JOURNAL_ICON = './icons/journal';
+const ON = '-icon-on.png';
+const OFF = '-icon-off.png';
+const NO_TRACKER_FILLED_OUT = 'no trackers filled out';
+const NO_NOTES_FILLED_OUT = 'no notes filled out';
+const NO_JOURNAL_FILLED_OUT = 'no journal filled out';
 
 /**
  * @module DailyLogPreview
@@ -28,101 +28,101 @@ const NO_JOURNAL_FILLED_OUT = "no journal filled out";
  */
 
 class DailyLogPreview extends HTMLElement {
-	/* Creates an HTML skeleton and defines the hierachy */
-	constructor() {
-		super();
+    /* Creates an HTML skeleton and defines the hierachy */
+    constructor() {
+        super();
 
-		/* Creates shadow DOM */
-		const shadow = this.attachShadow({ mode: "open" });
+        /* Creates shadow DOM */
+        const shadow = this.attachShadow({ mode: 'open' });
 
-		/* Creates all elements of the skeleton */
-		const wrapper = document.createElement("section"); // outer container div
-		const leftSideDiv = document.createElement("div"); // contains title and preview text
-		const rightSideDiv = document.createElement("div"); // contains the trackers
-		const title = document.createElement("h2");
-		const preview = document.createElement("p");
-		const trackerIcon = document.createElement("img");
-		const notesIcon = document.createElement("img");
-		const journalIcon = document.createElement("img");
+        /* Creates all elements of the skeleton */
+        const wrapper = document.createElement('section'); // outer container div
+        const leftSideDiv = document.createElement('div'); // contains title and preview text
+        const rightSideDiv = document.createElement('div'); // contains the trackers
+        const title = document.createElement('h2');
+        const preview = document.createElement('p');
+        const trackerIcon = document.createElement('img');
+        const notesIcon = document.createElement('img');
+        const journalIcon = document.createElement('img');
 
-		/* Defines the heirachy */
-		leftSideDiv.appendChild(title);
-		leftSideDiv.appendChild(preview);
-		rightSideDiv.appendChild(trackerIcon);
-		rightSideDiv.appendChild(notesIcon);
-		rightSideDiv.appendChild(journalIcon);
-		wrapper.appendChild(leftSideDiv);
-		wrapper.appendChild(rightSideDiv);
-		shadow.appendChild(wrapper);
+        /* Defines the heirachy */
+        leftSideDiv.appendChild(title);
+        leftSideDiv.appendChild(preview);
+        rightSideDiv.appendChild(trackerIcon);
+        rightSideDiv.appendChild(notesIcon);
+        rightSideDiv.appendChild(journalIcon);
+        wrapper.appendChild(leftSideDiv);
+        wrapper.appendChild(rightSideDiv);
+        shadow.appendChild(wrapper);
 
-		/* Apply external styles to the shadow dom */
-		const dailyLogPreviewCSS = document.createElement("link");
-		dailyLogPreviewCSS.setAttribute("rel", "stylesheet");
-		dailyLogPreviewCSS.setAttribute(
-			"href",
-			"./styles/daily-log-preview.css"
-		);
-		shadow.appendChild(dailyLogPreviewCSS);
+        /* Apply external styles to the shadow dom */
+        const dailyLogPreviewCSS = document.createElement('link');
+        dailyLogPreviewCSS.setAttribute('rel', 'stylesheet');
+        dailyLogPreviewCSS.setAttribute(
+            'href',
+            './styles/daily-log-preview.css',
+        );
+        shadow.appendChild(dailyLogPreviewCSS);
 
-		/* Adds classes to the created elements */
-		wrapper.setAttribute("class", DAILY_LOG_PREVIEW_WRAPPER_CLASS);
-		leftSideDiv.setAttribute("class", TEXT_WRAPPER_CLASS);
-		rightSideDiv.setAttribute("class", ICONS_WRAPPER_CLASS);
+        /* Adds classes to the created elements */
+        wrapper.setAttribute('class', DAILY_LOG_PREVIEW_WRAPPER_CLASS);
+        leftSideDiv.setAttribute('class', TEXT_WRAPPER_CLASS);
+        rightSideDiv.setAttribute('class', ICONS_WRAPPER_CLASS);
 
-		/* By default set each icon to its png in off mode */
-		trackerIcon.setAttribute("src", `${TRACKER_ICON}${OFF}`);
-		trackerIcon.setAttribute("alt", NO_TRACKER_FILLED_OUT);
-		notesIcon.setAttribute("src", `${NOTES_ICON}${OFF}`);
-		notesIcon.setAttribute("alt", NO_NOTES_FILLED_OUT);
-		journalIcon.setAttribute("src", `${JOURNAL_ICON}${OFF}`);
-		journalIcon.setAttribute("alt", NO_JOURNAL_FILLED_OUT);
-		const icons = rightSideDiv.children;
-		for (let iconInd = 0; iconInd < icons.length; iconInd++) {
-			const icon = icons[iconInd];
-			icon.setAttribute("class", "log-icon");
-		}
+        /* By default set each icon to its png in off mode */
+        trackerIcon.setAttribute('src', `${TRACKER_ICON}${OFF}`);
+        trackerIcon.setAttribute('alt', NO_TRACKER_FILLED_OUT);
+        notesIcon.setAttribute('src', `${NOTES_ICON}${OFF}`);
+        notesIcon.setAttribute('alt', NO_NOTES_FILLED_OUT);
+        journalIcon.setAttribute('src', `${JOURNAL_ICON}${OFF}`);
+        journalIcon.setAttribute('alt', NO_JOURNAL_FILLED_OUT);
+        const icons = rightSideDiv.children;
+        for (let iconInd = 0; iconInd < icons.length; iconInd++) {
+            const icon = icons[iconInd];
+            icon.setAttribute('class', 'log-icon');
+        }
 
-		/* A function to fill in the components with data */
-		this.populateFields = function (
-			dateOfEntry,
-			textEntry,
-			didTrackers,
-			didNotes,
-			didJournal
-		) {
-			title.textContent = DAILY_LOG_TITLE + dateOfEntry;
-			preview.textContent = this.getPreviewText(textEntry);
+        /* A function to fill in the components with data */
+        this.populateFields = (
+            dateOfEntry,
+            textEntry,
+            didTrackers,
+            didNotes,
+            didJournal,
+        ) => {
+            title.textContent = DAILY_LOG_TITLE + dateOfEntry;
+            preview.textContent = this.getPreviewText(textEntry);
 
-			/* depending on if item done, show icon as on or off by updating src */
-			if (didTrackers) {
-				trackerIcon.setAttribute("src", `${TRACKER_ICON}${ON}`);
-			} else {
-				trackerIcon.setAttribute("src", `${TRACKER_ICON}${OFF}`);
-			}
-			if (didNotes) {
-				notesIcon.setAttribute("src", `${NOTES_ICON}${ON}`);
-			} else {
-				notesIcon.setAttribute("src", `${NOTES_ICON}${OFF}`);
-			}
-			if (didJournal) {
-				journalIcon.setAttribute("src", `${JOURNAL_ICON}${ON}`);
-			} else {
-				journalIcon.setAttribute("src", `${JOURNAL_ICON}${OFF}`);
-			}
-		};
+            /* depending on if item done, show icon as on or off by updating src */
+            if (didTrackers) {
+                trackerIcon.setAttribute('src', `${TRACKER_ICON}${ON}`);
+            } else {
+                trackerIcon.setAttribute('src', `${TRACKER_ICON}${OFF}`);
+            }
+            if (didNotes) {
+                notesIcon.setAttribute('src', `${NOTES_ICON}${ON}`);
+            } else {
+                notesIcon.setAttribute('src', `${NOTES_ICON}${OFF}`);
+            }
+            if (didJournal) {
+                journalIcon.setAttribute('src', `${JOURNAL_ICON}${ON}`);
+            } else {
+                journalIcon.setAttribute('src', `${JOURNAL_ICON}${OFF}`);
+            }
+        };
 
-		/* A function to get the preview text */
-		this.getPreviewText = function (textEntry) {
-			if (!textEntry) {
-				return NO_PREVIEW_TEXT;
-			}
-			return textEntry.length > MAX_PREVIEW_LENGTH
-				? textEntry.substring(0, MAX_PREVIEW_LENGTH)
-				: textEntry;
-		};
-	}
+        /* A function to get the preview text */
+        this.getPreviewText = (textEntry) => {
+            if (!textEntry) {
+                return NO_PREVIEW_TEXT;
+            }
+            return textEntry.length > MAX_PREVIEW_LENGTH
+                ? textEntry.substring(0, MAX_PREVIEW_LENGTH)
+                : textEntry;
+        };
+    }
 }
 
-customElements.define("daily-log-preview", DailyLogPreview);
+customElements.define('daily-log-preview', DailyLogPreview);
 
 export default DailyLogPreview;

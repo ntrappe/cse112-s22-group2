46f7a61c07c05f35b0f0481cf3d4543b05436414
b1hu <jackhubaitong@gmail.com>
2022-06-07 14:50:36 -0700

    daily log test
    weird bug on click event

--- testing/cypress/daily-log-test.js
+++ testing/cypress/daily-log-test.js
@@ -41,6 +41,71 @@ describe('Check default contents of daily log', { includeShadowDom: true }, () =
             expect($el).to.have.attr('placeholder', 'Click to start typing...');
         });
     });
+
+    it('note should have placeholder text', () => {
+        cy.get('#notes-placeholder').then(($el) => {
+            expect($el).to.contain('Click to add a note...');
+        });
+
+    });
+
+    it('cancel button and save button exist', () => {
+        cy.get('.control-btn');
+    });
+
+    it('tracker button exist', () => {
+        cy.get('#tracker-container');
+    });
+
+    it('note component exist', () => {
+        cy.get('#notes-container');
+        cy.get('#notes-container').find('h3');
+        cy.get('#notes-placeholder');
+    });
+
+    it('journal component exist', () => {
+        cy.get('#journal-container');
+        cy.get('#journal-text');
+    });
+});
+
+describe('Note container function', { includeShadowDom: true }, () =>{
+
+    it('test click on placeholder', () => {
+        cy.get('#notes-placeholder').click();
+    });
+
+    it('test content block create', () =>{
+        cy.get('#notes-placeholder')
+        .click()
+        .then(() => {
+            cy.get('#notes-container').find('#notes-placeholder').should('have.css', 'display', 'none');
+        });
+        cy.get('note-container').find('span');
+        cy.get('note-container > span').eq(0).type('testing');
+        cy.get('note-container > span').eq(0).type('Cypress.io{enter}')
+        .then(() => {
+            cy.get('note-container > span').eq(1);
+        });
+    });
+    it('test content block delete', () => {
+        cy.get('note-container > span').eq(1).type('Cypress.io{backspace}')
+        .then(() => {
+            cy.get('note-container > span').eq(1).should('not.exist');
+        });
+    });
+});
+
+describe('Journal function', {includeShadowDom: true}, () =>{
+    const test_content = 'testing content';
+
+    it('call function to set contents', () => {
+        dailyLog.populateFields('', '', [], test_content);
+    });
+
+    it('testing content', () =>{
+        cy.get('#journal-text').contains(test_content);
+    })
 });
 
 describe('Set and check daily log contents', { includeShadowDom: true }, () => {
